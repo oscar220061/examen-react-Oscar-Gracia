de11fb1ca56712c06665cbabb6db14a7967733b2
Óscar Gracia Sánchez <220061@iessanalberto.com>
2023-11-28 13:54:32 +0100

    ejercicio4

--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,94 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+const initialState = {
 
+  texto: '',    
+  verificacionUsuario: false,
+  verificacionPassword: false,
+  singOut: false
+
+
+
+};
+const reducer = (state, action) =>{
+  switch(action.type){
+    case "setTexto":
+            return {...state, texto: action.payload};
+    case "setVerificacionUsuario":
+        return {...state, verificacionUsuario: !state.verificacionUsuario};
+    case "setVerificacionPassword":
+        return {...state, verificacionPassword: !state.verificacionPassword}
+    case "setSingOut":
+      return {...state, singOut: !state.singOut}
+    default:
+      return state;
+  }
+}
 function Login() {
     
-const [ username, setUsername ] = useState('')
 
-function handleUsername(e){
-    setUsername(e.target.value);
+const [state, dispatch] = useReducer(reducer, initialState);
+const isValid = state.verificacionPassword && state.verificacionUsuario;
+let signInOk = false;
 
+
+  function verificarPassword(event){
+    dispatch({type: "setTexto", payload:event.target.value})
+    if(state.texto.length > 0){
+      dispatch({type: "setVerificacionPassword"})
+    }
+  }
+  function verificarUsuario(event){
+    dispatch({type: "setTexto", payload:event.target.value})
+    if(state.texto.length > 0){
+      dispatch({type: "setVerificacionUsuario"})
+    }
+  }
+
+
+function submitFormHandler(event){
+  event.preventDefault();
+  if (!isValid) {
+    return alert('Invalid form inputs!');
+    
+  }else{
+    signInOk = true;
+    return alert('Correct LogIn!');
+  }
 }
 
+function signUp(event){
+  event.preventDefault();
+  if (!isValid) {
+    return alert('Invalid form inputs!');
+    
+  }else{
+  
+    return alert('correct SingUp!');
+  }
+}
+
+  function signOut(){
+    if(!signInOk){
+      return alert('Ninguna sesion iniciada');
+  
+    }else{
+      return alert('Sesion cerrada correctamente!');
+    }
+  }
+
+
+
   return (
-    <div className="login">
-    <form onSubmit={e => { }}>
+    <div className="login" >
+    <form onSubmit={submitFormHandler}>
       <label htmlFor="login-username">Username:</label>
-      <input type="text" value={username} onChange={handleUsername} name="login-username" id="login-username" />
+      <input type="text" onChange={verificarUsuario} name="login-username" id="login-username" />
       <label htmlFor="login-password">Password:</label>
-      <input type="password" name="login-password" id="login-password" />
-      <input type="submit" value="Sign In" disabled={username.length === 0} />
+      <input type="password" onChange={verificarPassword} name="login-password" id="login-password" />
+      <input type="submit" value="Sign In"  />
     </form>
-    <div className="button-div"><button>Sign Up</button></div>
-    <div className="button-div"><button>Sign Out</button></div>
+    <div className="button-div"><button onClick={signUp}>Sign Up</button></div>
+    <div className="button-div"><button onClick={signOut}>Sign Out</button></div>
     
     </div>
   )
